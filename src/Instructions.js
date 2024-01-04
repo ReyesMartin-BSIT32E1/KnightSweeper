b12646008823ffe69b9c7bf9878917e54b5947c6
Smarty0917 <martintristanmreyes@gmail.com>
2024-01-04 17:44:13 +0800

    mobile fix

--- src/Instructions.js
+++ src/Instructions.js
@@ -4,7 +4,7 @@ const Instructions = ({ onBack }) => {
   return (
     <div className="menu-screen">
       <h1 id="instructions"></h1>
-      <img src="./sprites/Instructions-Page.png" style={{width: "clamp(768px, 60%, 60%)",}}/>
+      <img src="./sprites/Instructions-Page.png" style={{width: "clamp(512px, 60%, 60%)",}}/>
       <button className='back-button' onClick={onBack}>◀</button>
     </div>
   );
